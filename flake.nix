d31ac3ef98913c2537d410832cdce2e56f5ed319
Marc Venturini <marc.venturini@gmail.com>
2026-08-20 00:00:44 +0800

    Added docker-compose

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [ pkgs.docker-compose ];
+      };
+    };
+}
